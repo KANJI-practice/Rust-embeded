c4c4717e93ceca4fc46a5d87ca357cd6bbfc67ef
MIYAWKAI <atoka11nm23@gmail.com>
2021-08-15 11:59:38 +0900

    qemuでHello World

---
+++ hello/runner.bat
@@ -0,0 +1,22 @@
+
+set "f=%1"
+call set "f=%%f:\=/%%"
+call set "f=%%f:C:=%%"
+
+set "d=%CARGO_MANIFEST_DIR%"
+call set "d=%%d\=/%%"
+call set "d=%%d:C%%"
+
+set "a = C:\\work\\embeded\\hello"
+@REM #%usr/local/bin/%
+
+docker run --rm -v %CARGO_MANIFEST_DIR%:/%d% ^
+    qemu ^
+    sh -c ^"cd /%d%; ^
+    qemu-system-gnuarmeclipse ^
+        -cpu cortex-m4 ^
+        -machine STM32F4-Discovery ^
+        -nographic ^
+        -semihosting-config enable=on,target=native ^
+        -kernel %f%"
+
